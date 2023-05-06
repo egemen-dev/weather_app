ed96b63a2e265785c8d2d676a273c9dc2e6d1762
egemen-dev <egemenwasd@gmail.com>
2023-05-06 23:00:42 +0300

    Import fontawesome to index js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/brands";
 import _ from "lodash";
 import "./style.css";
 import search from "./logic/search";
