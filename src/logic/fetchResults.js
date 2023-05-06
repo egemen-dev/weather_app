4710dbd98b2222183ed68e0751013453a28c7883
egemen-dev <egemenwasd@gmail.com>
2023-05-06 23:03:05 +0300

    Modify fetch results - calculate and add average temps to json

--- src/logic/fetchResults.js
+++ src/logic/fetchResults.js
@@ -1,13 +1,26 @@
+import average from "../integerFunctions/average";
+
 export default async function fetchResults(lat, lng) {
   try {
+    let averageTemps = [];
+
     addLoadingAnimation();
 
     const response = await fetch(
       `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&daily=temperature_2m_max&daily=temperature_2m_min&timezone=GMT&daily=weathercode`
     );
 
+    const json = await response.json();
+
+    json.daily.temperature_2m_max.forEach((maxTemp, index) => {
+      let averageTemp = average(maxTemp, json.daily.temperature_2m_min[index]);
+      averageTemps.push(averageTemp);
+    });
+
+    json.averageTemps = averageTemps;
+
     removeLoadingAnimation();
-    return await response.json();
+    return json;
   } catch (error) {
     console.log(error);
   }
