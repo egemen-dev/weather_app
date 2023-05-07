6cc4c0a9209b8ceefae8e9537894a868e2d82b23
egemen-dev <egemenwasd@gmail.com>
2023-05-07 03:04:42 +0300

    Update layout

--- src/logic/displayResults.js
+++ src/logic/displayResults.js
@@ -11,18 +11,20 @@ export default function displayResults(results) {
     weatherList.insertAdjacentHTML(
       "beforeend",
       `<div class="grid grid-cols-8 flex justify-around">
-      <div class="flex justify-center text-white text-xs lg:text-lg col-span-2">${
+      <div class="flex justify-center text-white font-light text-xs lg:text-xl lg:font-medium col-span-2 items-center">${
         weatherResults.time[index]
       }</div>
-      <div class="flex justify-center col-span-1">
+      <div class="flex justify-center col-span-1 items-center">
         <i class="fa-solid fa-${
           weatherSymbols[weatherResults.weathercode[index]]
-        } fa-2xl" style="color: #ffffff"></i>
+        } fa-xl" style="color: #ffffff"></i>
       </div>
-      <div class="flex justify-center text-white text-xs lg:text-lg col-span-4">
-        <p>${weatherDescriptions[weatherResults.weathercode[index]]}</p>
+      <div class="flex justify-center content-center text-white text-xs lg:text-xl col-span-3 items-center">
+        <p class="text-center">${
+          weatherDescriptions[weatherResults.weathercode[index]]
+        }</p>
       </div>
-      <div class="flex justify-center text-white text-xs lg:text-lg col-span-1">${temperature} °C</div>
+      <div class="flex justify-center text-white text-xs lg:text-xl col-span-2 items-center">${temperature} °C</div>
     </div>`
     );
   });
